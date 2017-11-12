1668aa33e27983d7d8654a3f7b85bfdaa3580452
lgants <lgants@gmail.com>
2017-11-12 11:41:08 -0600

    implemented quick refactors

--- src/client/client.js
+++ src/client/client.js
@@ -10,13 +10,14 @@ import { renderRoutes } from 'react-router-config';
 import Routes from './Routes';
 import reducers from './reducers';
 
+// Use initial state value set in a script inside renderer.js
 const store = createStore(
   reducers,
   window.INITIAL_STATE,
   applyMiddleware(thunk)
 );
 
-// Need to render the app into the same div the app was initial rendered into
+// Need to render the app into the same div the app was initially rendered into
 // BrowserRouter doesn't work on server since it's hardcoded to use an address bar, which isn't available on the server
 ReactDOM.hydrate(
   <Provider store={store}>
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -3,6 +3,7 @@ import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
+import serialize from 'serialize-javascript';
 import Routes from '../client/Routes';
 
 // Store var will possess initial state (i.e. all load data functions already called)
@@ -15,14 +16,14 @@ export default (req, store) => {
     </Provider>
   );
 
-  // need to manually include script tag to ensure the browser goes back to the server to request the bundle
+  // Need to manually include script tag to ensure the browser goes back to the server to request the bundle
   return `
     <html>
       <head></head>
       <body>
         <div id="root">${content}</div>
         <script>
-          window.INITIAL_STATE = ${JSON.stringify(state.getState())}
+          window.INITIAL_STATE = ${serialize(state.getState())}
         </script>
         <script src="bundle.js"></script>
       </body>
